89faf7ce9e4a54d26cf22a85c082c9a883b0c924
akhilus <71842429+akhilus@users.noreply.github.com>
2020-10-02 21:24:11 +0530

    Update pakExtractorbyMerwick.sh

--- pakExtractorbyMerwick.sh
+++ pakExtractorbyMerwick.sh
@@ -19,9 +19,9 @@ printf 'if u know something teach to others'
 
 echo "This script is uses to Install Pak Extractor tool in termux app"
 echo "please subscribe to my channel merwick gaming"
-pkg install x11-repo
+pkg install x11-repo -y
 
-pkg install qemu-system-i386
+pkg install qemu-system-i386 -y
 
-pkg install qemu-user-i386
+pkg install qemu-user-i386 -y
 
